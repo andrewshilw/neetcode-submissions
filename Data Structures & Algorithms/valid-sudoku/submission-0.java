3f0ab9fa10815a4e170c03ac60ecf1cde32d1e02
Andrew Shi <42019625+andrewshilw@users.noreply.github.com>
2026-09-20 16:21:29 +0800

    Update: valid-sudoku - submission-0

--- Data Structures & Algorithms/valid-sudoku/submission-0.java
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -1,27 +1,81 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<Integer>[] sqr = new HashSet[board.length];
-        Set<Integer>[] row = new HashSet[board.length];
-        Set<Integer>[] col = new HashSet[board[0].length];
         for (int i = 0; i < 9; i++) {
-            sqr[i] = new HashSet<>();
-            row[i] = new HashSet<>();
-            col[i] = new HashSet<>();
+            if (!isValidRow(board, i)) {
+                return false;
+            }
         }
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (Character.isDigit(board[i][j])) {
-                    int c = Character.getNumericValue(board[i][j]);
-                    if(sqr[i/3 * 3 + j/3].contains(c) ||
-                    row[i].contains(c) || col[j].contains(c))
-                        return false;
-                    sqr[i/3 * 3 + j/3].add(c);
-                    row[i].add(c);
-                    col[j].add(c);
+        for (int j = 0; j < 9; j++) {
+            if (!isValidCol(board, j)) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!isValidSub(board, i, j)) {
+                    return false;
                 }
             }
         }
+
+        return true;
+    }
+
+    private boolean isValidRow(char[][] board, int row) {
+        Set<Character> set = new HashSet<>();
+
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == '.') {
+                continue;
+            }
+
+            if (set.contains(board[row][j])) {
+                return false;
+            }
+
+            set.add(board[row][j]);
+        }
+
+        return true;
+    }
+
+    private boolean isValidCol(char[][] board, int col) {
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == '.') {
+                continue;
+            }
+
+            if (set.contains(board[i][col])) {
+                return false;
+            }
+
+            set.add(board[i][col]);
+        }
+
+        return true;
+    }
+
+    private boolean isValidSub(char[][] board, int i, int j) {
+        Set<Character> set = new HashSet<>();
+
+        for (int s = i; s < i + 3; s++) {
+            for (int t = j; t < j + 3; t++) {
+                if (board[s][t] == '.') {
+                    continue;
+                }
+
+                if (set.contains(board[s][t])) {
+                    return false;
+                }
+
+                set.add(board[s][t]);
+            }
+        }
+
         return true;
     }
 }
